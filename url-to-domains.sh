66007b742b202fc28f3bdda62ef6f37bf567cf7f
Jeroen Simonetti <jsimonetti@users.noreply.github.com>
2023-04-19 21:05:22 +0200

    Update url-to-domains.sh

--- url-to-domains.sh
+++ url-to-domains.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 INPUT="doh_server_urls.list"
 sed -e 's/.*:\/\///g' -e 's/\/.*$//g' $INPUT > doh_server_domains.list
+sort -u doh_server_domains.list > doh_server_domains.list.sorted && mv doh_server_domains.list.sorted doh_server_domains.list
