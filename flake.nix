898df42198aac0e1c724391de5ed11991ae072b2
Jeroen Simonetti <jeroen@simonetti.nl>
2023-04-24 03:35:22 +0200

    Update flake format
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- flake.nix
+++ flake.nix
@@ -1,34 +1,18 @@
-  {
+{
   description = "Public DOH Servers";
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs, ... }:
-    let
-      # System types to support.
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
 
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      packages = {
+        default = self.packages.${system}.public_doh_servers;
 
-    in {
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-         public_doh_servers = pkgs.stdenv.mkDerivation {
-          name = "public_doh_servers";
+        public_doh_servers = nixpkgs.legacyPackages.${system}.callPackage self {
           src = self;
-          phases = [ "installPhase" ];
-          installPhase = ''
-            mkdir $out;
-            cp $src/*.list $out;
-            cp $src/*.set $out;
-          '';
         };
-        });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.public_doh_servers);
-    };
+      };
+    });
 }
